428d792c4d58f821d20e0e46e16183c55f02575f
Yury <yujika87@gmail.com>
2021-10-07 23:19:17 +0300

    add js 5

---
+++ CW/5_sort_odd.js
@@ -0,0 +1,14 @@
+function sortArray(array) {
+  let result = []
+  for (let i=0; i<array.length; i++) {
+    if (array[i] % 2 === 1) {
+      let temp = array[i]
+      for (let j=0; j<array.length; j++) {
+        
+        if ((array[j] % 2 === 1) && (array[j] < temp)) {
+          
+        }
+      }
+    }
+    return
+  }
