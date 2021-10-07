7d0e6b6d7fa055424cf31bb81bd10735fdfd3232
Yury <yujika87@gmail.com>
2021-10-08 00:54:11 +0300

    add js 5

--- CW/5_sort_odd.js
+++ CW/5_sort_odd.js
@@ -3,7 +3,7 @@ function sortArray(array) {
   for (let i=0; i<array.length; i++) {
     if (array[i] % 2 !== 0) {
       let minVal = array[i]
-      for (let j=i+1; j<=array.length; j++) {
+      for (let j=i+1; j<array.length; j++) {
         
         if ((array[j] % 2 !== 0) && (array[j] < minVal)) {
           
